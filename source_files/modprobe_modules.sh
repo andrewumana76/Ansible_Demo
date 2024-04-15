46202956a87468ee916556df33c43d4c19924abc
andrewumana76 <andrewumana76@gmail.com>
2024-04-15 06:33:49 -0400

    Logic update to modules script

--- source_files/modprobe_modules.sh
+++ source_files/modprobe_modules.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if !(grep -r "$1" /etc/modprobe.conf 2> /dev/null || grep -r "$1" /etc/modprobe.d/* 2> /dev/null) ;
+if !(grep -r "$1" /etc/modprobe.conf 2> /dev/null || grep -r "$1.conf" /etc/modprobe.d/* 2> /dev/null) ;
 then
 	echo "Creating /etc/modprobe.d/$1.conf"
 	cat > /etc/modprobe.d/$1.conf << EOF
-install $1 /bin/false
+install $1 /bin/$2
 blacklist $1
 EOF
 
